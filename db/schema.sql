a7709a3d2264d3f2196e9baae9bcff67a59ad425
Arpana <arpanageorge@yahoo.in>
2019-04-14 21:28:48 -0700

    updated files

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS RealWize_DB;
 -- Create the RealWize_DB database --
 CREATE DATABASE RealWize_DB;
 USE RealWize_DB;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,56 @@
-var db = require("../models");
+ var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+ module.exports = function(app) {
+//   // Get all examples
+   app.get("/", function(req, res) {
+    //db.Example.findAll({}).then(function(dbExamples) {
+        var hbsObject = {
+            // value: "hello"
+           
+          };
+        res.render("index", hbsObject);
+    //});
+  });
+
+
+  // POST route for saving a new todo
+  app.post("/api/homeList", function(req, res) {
+    // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just pass in object houseDetails
+    db.Housing.create({
+        address: req.body.homeAddress,
+        Year_built: req.body.yearBuilt,
+        lot_size: req.body.lotSize,
+        propclass: req.body.propClass,
+        date_listed: req.body.date_listed,
+        rooms_amenities: req.body.roomsAmenities,
+     }).then(function(results) {
+        // `results` here would be the newly created row
+        //res.redirect("/api/homeList");
+        //res.end();
+        res.json(results);
+      }).catch(err => {
+        
+      })
+   });
+
+  // GET route for getting all of the homeList
+  app.get("/api/homeList", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Housing.findAll({}).then(function(dbProperty) {
+        
+      // We have access to the homeList as an argument inside of the callback function
+      var propertyListObject = {
+        list: dbProperty
+       
+      };
+      
+      console.log("from db server side"+dbProperty);
+      //res.json(dbProperty);
+      res.render("index", propertyListObject);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+}
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
+
