f3ef525bbeaa9ee1a1ef2029a27c88bf48060ae7
Phi Hai Nguyen <phihai93@gmail.com>
2019-04-16 12:40:06 -0700

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS RealWize_DB;
+DROP DATABASE IF EXISTS Sequelize_Passport;
 -- Create the RealWize_DB database --
 CREATE DATABASE RealWize_DB;
 USE RealWize_DB;
+-- Create the Sequelize_Passport; database --
 CREATE DATABASE Sequelize_Passport;
 USE Sequelize_Passport;
