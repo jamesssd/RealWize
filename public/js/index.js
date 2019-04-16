c42fe65cbc83f1280f21879e928196813afe7d01
Arpana <arpanageorge@yahoo.in>
2019-04-16 16:44:02 -0700

    db password

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
- 
   // console.log("hello");
   //var homeListArray = [];
   // Getting a reference to the input field where user adds a city name
   var $cityInput = $("input.search-city");
-
+  // Our new todos will go inside the todoContainer
+  //var $propertListContainer = $(".list-container");
+  // Adding event listeners for deleting, editing, and adding 
+  // $(document).on("click", "button.delete", deleteTodo);
+  // $(document).on("click", "button.complete", toggleComplete);
+  // $(document).on("click", ".todo-item", editTodo);
+  // $(document).on("keyup", ".todo-item", finishEdit);
+  // $(document).on("blur", ".todo-item", cancelEdit);
   $(document).on("submit", "#todo-form", getPropertyListApi);
 
   // document.getElementById("myBtn").addEventListener("click", getPropertyListApi);
@@ -16,26 +22,22 @@ $(document).ready(function() {
     console.log("inserting values");
     event.preventDefault();
    //setting header info to send it as the last argument in axios get request
-   
-   //===================================================
-   
-   var config = {
+    var config = {
       headers: 
       {
         'accept': 'application/json',
         'apikey' : 'dcc3e13d6cf56f0afa62028c6856b7a7'
         }
     };
-//=========================================================
 
     // Getting the value from city textbox
     var cityName = $cityInput.val().trim();
     $cityInput.val("");
     console.log("cityName client"+cityName);
 
-    var cityObject = {
-      searchCity : cityName
-    };
+    // var cityObject = {
+    //   searchCity : cityName
+    // };
     // $.post("/api/homeListBasedCity/bothell", cityObject,function(){
       
     // })
@@ -43,35 +45,102 @@ $(document).ready(function() {
    
 
     //  axios get method to get api snapshot data    
-    axios.get('https://search.onboard-apis.com/propertyapi/v1.0.0/property/snapshot?cityname='+cityName, config)
-    .then(function (response) {
-     // handle success
-      console.log(response);
-      for (i in response.data.property) {
+    // axios.get('https://search.onboard-apis.com/propertyapi/v1.0.0/property/snapshot?cityname='+cityName, config)
+    // .then(function (response) {
+    //  // handle success
+    //   console.log(response);
+    //   for (i in response.data.property) {
   
-       //Getting bedbath
-         var bedNumbers = response.data.property[i].building.rooms.bathstotal;
-        var bathNumbers = response.data.property[i].building.rooms.beds;
-        var bedbath =  bedNumbers+"beds"+bathNumbers+"baths";
-        //console.log("address"+v);
-        var houseDetails = {
-          homeAddress: response.data.property[i].address.oneLine,
-          cityName: response.data.property[i].address.locality,
-          //  propertyID: response.data.property[i].identifier.obPropId
-          yearBuilt: response.data.property[i].summary.yearbuilt,
-          lotSize: response.data.property[i].lot.lotSize1,
-          propClass: response.data.property[i].summary.propclass,
-          date_listed: response.data.property[i].vintage.pubDate,
-          // text: $newItemInput.val().trim(),
-          roomsAmenities: bedbath
-        };//end of var housedetails
+    //    //Getting bedbath
+    //      var bedNumbers = response.data.property[i].building.rooms.bathstotal;
+    //     var bathNumbers = response.data.property[i].building.rooms.beds;
+    //     var bedbath =  bedNumbers+"beds"+bathNumbers+"baths";
+    //     //console.log("address"+v);
+    //     var houseDetails = {
+    //       homeAddress: response.data.property[i].address.oneLine,
+    //       cityName: response.data.property[i].address.locality,
+    //        propertyID: response.data.property[i].identifier.obPropId
+    //       yearBuilt: response.data.property[i].summary.yearbuilt,
+    //       lotSize: response.data.property[i].lot.lotSize1,
+    //       propClass: response.data.property[i].summary.propclass,
+    //       date_listed: response.data.property[i].vintage.pubDate,
+    //       // text: $newItemInput.val().trim(),
+    //       roomsAmenities: bedbath
+    //     };
+
+              
     
         // POST route for saving a new todo
         // This function inserts a new todo into our database and then updates the view
-        $.post("/api/homeList", houseDetails,function(){
+      //   $.post("/api/homeList", houseDetails,function(){
       
-       })
-     }//end of for loop
+      //  })//end of $.post("/api/homeList"
+
+          //  axios get method to get api school data
+
+          // axios.get('https://search.onboard-apis.com/propertyapi/v1.0.0/property/detailwithschools?id='+houseDetails.propertyID, config)
+    // .then(function (response2) {
+    //  // handle success
+    //   console.log(response);
+           
+    //     //console.log("address"+v);
+    //     var houseDetails = {
+    //      
+              // propId : response2.data.property[0].identifier.obPropId,
+   
+    //       lotSize: response2.data.property[0].lot.lotSize2,
+        //Getting bedbath
+    //      var bathNumber = response2.data.property[0].building.rooms.bathstotal;
+    //     var bedNumber = response2.data.property[0].building.rooms.beds;
+    // var roomTotal = response2.data.property[0].building.rooms.roomsTotal;
+    
+              //utilities
+              // heating: response2.data.property[0].utilities.heatingtype,
+              //parking
+
+    //       garageType: response2.data.property[0].building.parking.garagetype;
+    
+    //     };
+
+              
+    
+        // POST route for saving a new todo
+        // This function inserts a new todo into our database and then updates the view
+      //   $.post("/api/homeList", houseDetails,function(){
+      
+      //  })//end of $.post("/api/homeList"
+
+          //  axios get method to get api school data
+
+          
+    //  }//end of for loop
+     
+   
+    // $.get("/api/homeListBasedCity/"+ cityName, function(data){
+    //   console.log(data)
+    //   // for (let i = 0; i < data.length; i++) {
+    //   //   $('#results').append(data[i].address + '<br>')
+        
+        
+    //   // }
+    // })
+    //       // This function grabs propertlists from the database and updates the view
+    //    // function getHomeList() {
+       
+  
+    //  })
+    // .catch(function (error) {
+    //  // handle error
+    //   console.log(error);
+    // })
+    // .then(function () {
+    //  // selectPropertyList();
+      
+    // });
+
+            // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    //  }//end of for loop
      
     window.location='/api/homeListBasedCity/' + cityName
     // $.get("/api/homeListBasedCity/"+ cityName, function(data){
@@ -94,7 +163,7 @@ $(document).ready(function() {
     // .then(function () {
     //  // selectPropertyList();
       
-     });//end of axios
+    // });
 
     // var todo = {
     //   text: $newItemInput.val().trim(),
@@ -103,7 +172,7 @@ $(document).ready(function() {
 
     // $.post("/api/todos", todo, getTodos);
     // $newItemInput.val("");
-  }// end of getPropertyListApi
+  } //End of getPropertListApi(event)
 
   //function selectPropertyList(){
     // $.get("/api/homeList", function(data) {
@@ -112,6 +181,4 @@ $(document).ready(function() {
     //   //initializeRows();
     // });
  // }
-})//end of document.ready
-//======MODAL CONTROL========//
-
+})
